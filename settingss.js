831ab18c42dbe6c7cc2359b0e28381c2e43b3eee
simpsoncarlos560-a11y <simpsoncarlos560@gmail.com>
2025-08-19 16:53:33 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "",  // + Project Status: Active
+! Version: V.1.5.0 Neon Edition
+# License: MITadd your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "263714757857", // +22893598205put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
