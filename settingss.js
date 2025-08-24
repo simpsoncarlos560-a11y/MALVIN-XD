20af61fc49c33909e83c662cf7e7a47820249c24
simpsoncarlos560-a11y <simpsoncarlos560@gmail.com>
2025-08-24 06:10:02 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "",  //malvin~8380ECrS#kn4HfakiAHUt3pBqFX8esKOj4z6EprAHZvbP-fWq0mo add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "263714757857",22893598205// put your phone number here
   
-  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  PREFIX: ".", auto status// prefix (e.g. , ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "Africa/Harare" Togo  //put your country timeZone....leave blank if u don't know.
 };
